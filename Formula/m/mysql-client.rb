44edb8011fba011ebe747e54b861e8a47e6cf18f
Bevan Kay <email@bevankay.me>
2025-01-21 21:24:53 +1100

    mysql-client 9.2.0
    mysql-client: add dependency

--- Formula/m/mysql-client.rb
+++ Formula/m/mysql-client.rb
@@ -1,8 +1,8 @@
 class MysqlClient < Formula
   desc "Open source relational database management system"
-  homepage "https://dev.mysql.com/doc/refman/9.1/en/"
-  url "https://cdn.mysql.com/Downloads/MySQL-9.1/mysql-9.1.0.tar.gz"
-  sha256 "52c3675239bfd9d3c83224ff2002aa6e286fab97bf5b2b5ca1a85c9c347766fc"
+  homepage "https://dev.mysql.com/doc/refman/9.2/en/"
+  url "https://cdn.mysql.com/Downloads/MySQL-9.2/mysql-9.2.0.tar.gz"
+  sha256 "a39d11fdf6cf8d1b03b708d537a9132de4b99a9eb4d610293937f0687cd37a12"
   license "GPL-2.0-only" => { with: "Universal-FOSS-exception-1.0" }
 
   livecheck do
@@ -33,12 +33,18 @@ class MysqlClient < Formula
 
   uses_from_macos "libedit"
 
-  # std::string_view is not fully compatible with the libc++ shipped
-  # with ventura, so we need to use the LLVM libc++ instead.
   on_ventura :or_older do
     depends_on "llvm@18"
     fails_with :clang
   end
+
+  on_linux do
+    depends_on "krb5"
+    depends_on "libtirpc"
+  end
+
+  # std::string_view is not fully compatible with the libc++ shipped
+  # with ventura, so we need to use the LLVM libc++ instead.
 
   fails_with :gcc do
     version "9"
